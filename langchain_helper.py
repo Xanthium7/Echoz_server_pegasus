30648daf880afd960117172af4e9c44df5243bfe
Xanthium7 <akshhaykmurali@gmail.com>
2025-01-10 19:06:25 +0530

    Add langchain_helper.py for context retrieval from vector database

---
+++ langchain_helper.py
@@ -0,0 +1,18 @@
+import os
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_relevent_context_from_db(query):
+    context = ""
+    embedding_function = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_db = Chroma(persist_directory="./chroma_db_nccn",
+                       embedding_function=embedding_function)
+    search_results = vector_db.similarity_search(query, k=6)
+    for result in search_results:
+        context += result.page_content + "\n"
+    print(context)
+    return context
